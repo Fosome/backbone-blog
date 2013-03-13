9b987e577bfd3e63d3a1c77ead8f9f6e2bc0a1da
Ryan Foster <ryan.foster@viget.com>
2013-03-13 10:23:24 -0400

    Init commit

---
+++ Gemfile
@@ -0,0 +1,20 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.12'
+gem 'pg'
+gem 'thin'
+gem 'sass-rails',     '~> 3.2.3'
+gem 'bootstrap-sass', '~> 2.3.0.1'
+
+group :development, :test do
+  gem 'pry'
+end
+
+# Gems used only for assets and not required
+# in production environments by default.
+group :assets do
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+gem 'jquery-rails'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BackboneBlog::Application.config.secret_token = '902e2500c36e6eac70e9444a263b096076182a6261e44deb44ae4c54753ad4f20c458e46e3983b4c281bee29e777ff88e73de1ab5dd88e2d01faa02a2e01203a'
